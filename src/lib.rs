999587fbc96004ca8c57b2675f176412699218d3
Riyansu <kenKEN.DEM@gmail.com>
2026-08-23 18:13:27 +0900

    fix ref of callee

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ use vm::run;
 
 pub use {
     api::ExternalApi,
-    ast::{Direction, Expr, Op, Side, Statement, Type, UnaryOp},
+    ast::{Callee, Direction, Expr, Op, Side, Statement, Type, UnaryOp},
     error::Error,
     vm::{Event, EventIterator},
 };
